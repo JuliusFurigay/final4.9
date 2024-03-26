81303847aab1c047fc59e6ba6fa892abb431126d
Julius Nathaniel A. Furigay <143113655+JuliusFurigay@users.noreply.github.com>
2024-03-26 12:18:37 +0800

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
 import { User } from '@/models/User';
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
