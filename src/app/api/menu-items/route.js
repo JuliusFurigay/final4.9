85bcdb9e34f60e557f14db16f90ea71681139baf
Julius Nathaniel A. Furigay <143113655+JuliusFurigay@users.noreply.github.com>
2024-03-26 12:08:21 +0800

    Update route.js

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -12,10 +12,10 @@ export async function POST(req) {
 }
 export async function PUT(req){
     mongoose.connect(process.env.MONGO_URL);
-    if (await isAdmin()){
+   
         const {_id, ...data} = await req.json();
     await MenuItem.findByIdAndUpdate(_id, data);
-    }
+    
     return Response.json(true);
 }
 
@@ -30,8 +30,8 @@ export async function DELETE(req){
     mongoose.connect(process.env.MONGO_URL);
     const url = new URL(req.url);
     const _id = url.searchParams.get('_id');
-    if (await isAdmin()) {
+   
         await MenuItem.deleteOne({_id});
-    }
+    
     return Response.json(true);
 }
