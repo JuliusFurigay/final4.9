b3b0f9f033b4b50905278976ac5269d1e9b018ea
Julius Nathaniel A. Furigay <143113655+JuliusFurigay@users.noreply.github.com>
2024-03-26 12:07:56 +0800

    Update route.js

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 import { MenuItem } from "../../../models/MenuItem";
-import { isAdmin } from "../auth/[...nextauth]/route";
 
 export async function POST(req) {
    mongoose.connect(process.env.MONGO_URL)
     const data = await req.json();
-    if (await isAdmin()) {
+   
         const menuItemDoc = await MenuItem.create(data);
         return Response.json(menuItemDoc);
-    } else {
-        return Response.json({});
-    }
+     
+        
 }
 export async function PUT(req){
     mongoose.connect(process.env.MONGO_URL);
